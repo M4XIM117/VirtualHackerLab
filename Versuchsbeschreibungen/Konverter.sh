f9d17a318166f23a7902b248c810d2a51a8677b4
Maxim Schmidt <maxim.schmidt@student.reutlingen-university.de>
2023-01-26 09:52:53 +0100

    Anpassungen

--- Versuchsbeschreibungen/Konverter.sh
+++ Versuchsbeschreibungen/Konverter.sh
@@ -2,7 +2,7 @@ echo "Starte Konvertierung..."
 for file in *.md
     echo "Konvertiere $file in www/Versuche/" 
 
-    pandoc $file -f markdown -t html5 -o ../www/Versuche/"${file%.*}.html" $file
+    pandoc $file -f markdown -t html5 -o ../www/Versuche/Anleitungen/"${file%.*}.html" $file
 done
 
 echo "feddig"
