d4ae17ca0813fe920f0b9b619b790f46500a42e5
Maxim Schmidt <maxim.schmidt@student.reutlingen-university.de>
2023-01-26 09:27:54 +0100

    Bash Skript angepasst

--- Versuchsbeschreibungen/Konverter.sh
+++ Versuchsbeschreibungen/Konverter.sh
@@ -2,8 +2,7 @@ echo "Starte Konvertierung..."
 for file in *.md
     echo "Konvertiere $file in www/Versuche/" 
 
-    pandoc $file -f markdown -t html5 
-
-end
+    pandoc $file -f markdown -t html5 -o ../www/Versuche/"${file%.*}.html" $file
+done
 
 echo "feddig"
