c7caa0e8f133ac7dbe5aa8f8637a0b1e1239fd93
Maxim Schmidt <maxim.schmidt@student.reutlingen-university.de>
2023-04-11 23:41:59 +0200

    fix

--- exercises/password_cracking/web-server/v2/create_users.sh
+++ exercises/password_cracking/web-server/v2/create_users.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for user in "cem" "nicole" "bilal" "maxim"; do 
-	useradd -rm -d /home/ubuntu -s /bin/bash -g root -G sudo -u $user; 
+	useradd -rm -d /home/ubuntu -s /bin/bash -g root -G sudo -u 1000 $user; 
 	password="$(shuf -n 1 /usr/share/wordlists/passwords.txt)"; 
 	echo "$user:$password" || chpasswd; 
 	echo Creating user "$user"...; 
