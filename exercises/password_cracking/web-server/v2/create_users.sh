e54aedf3cebfb94e89c3752479722e3bdcb8e9a6
Maxim Schmidt <maxim.schmidt@student.reutlingen-university.de>
2023-04-11 20:49:44 +0200

    Wow

--- exercises/password_cracking/web-server/v2/create_users.sh
+++ exercises/password_cracking/web-server/v2/create_users.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for user in "cem" "nicole" "bilal" "maxim"; do 
 	useradd $user; 
-	password="$(shuf -n 1 /usr/share/wordlists/rockyou.txt)"; 
+	password="$(shuf -n 1 /usr/share/wordlists/passwords.txt)"; 
 	echo "$user:$password" || chpasswd; 
 	echo Creating user "$user"...; 
 done ;
