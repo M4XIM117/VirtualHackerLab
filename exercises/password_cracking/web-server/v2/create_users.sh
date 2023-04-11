07395890385afe52ddbc32a2cc806dbeebc2dd3a
Maxim Schmidt <maxim.schmidt@student.reutlingen-university.de>
2023-04-11 18:43:24 +0200

    test

--- exercises/password_cracking/web-server/v2/create_users.sh
+++ exercises/password_cracking/web-server/v2/create_users.sh
@@ -1,4 +1,4 @@
-USERNAMES="Cem" "Nicole" "Bilal" "Maxim"
+USERNAMES=("Cem" "Nicole" "Bilal" "Maxim")
 for user in $USERNAMES; do 
 	useradd \$user; 
 	password=\$(shuf -n 1 /usr/share/wordlists/fasttrack.txt); 
