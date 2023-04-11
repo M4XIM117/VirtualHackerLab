2827c010b8900e909cc5c274623e27548c1b2aba
Maxim Schmidt <maxim.schmidt@student.reutlingen-university.de>
2023-04-11 19:13:47 +0200

    script FERTIG

--- exercises/password_cracking/web-server/v2/create_users.sh
+++ exercises/password_cracking/web-server/v2/create_users.sh
@@ -2,7 +2,7 @@ for user in "cem" "nicole" "bilal" "maxim"; do
 	useradd $user; 
 	password="$(shuf -n 1 /usr/share/wordlists/fasttrack.txt)"; 
 	echo "$user:$password" || chpasswd; 
-	echo Creating user \"$user"...; 
+	echo Creating user "$user"...; 
 done ;
 
 echo 'All done!'
