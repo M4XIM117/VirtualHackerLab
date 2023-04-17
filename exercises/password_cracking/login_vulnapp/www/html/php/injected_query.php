be5da1336d3743027b0434f544f1a21a524a441c
Maxim Schmidt <maxim.schmidt@student.reutlingen-university.de>
2023-04-17 16:36:26 +0200

    test

--- exercises/password_cracking/login_vulnapp/www/html/php/injected_query.php
+++ exercises/password_cracking/login_vulnapp/www/html/php/injected_query.php
@@ -16,7 +16,7 @@ if ($conn->connect_error) {
 $name = $_POST['name'];
 
 // Build and execute MySQLi query
-$sql = "SELECT * FROM user WHERE username = '$username' AND password_hash = '$password_hashed'";
+$sql = "SELECT * FROM user WHERE username = '$username' AND password_hash = '$name'";
 $result = mysqli_query($conn, $sql);
 
 // Display the result
