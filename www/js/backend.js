e3c61fdf7a9b8001269d39ba0fd03581b69ce696
Maxim Schmidt <maxim.schmidt@student.reutlingen-university.de>
2023-05-09 19:09:35 +0200

    clear

--- www/js/backend.js
+++ www/js/backend.js
@@ -17,6 +17,7 @@ var ptyProcess = pty.spawn(shell, [], {
 wss.on('connection', ws => {
     console.log("new session")
     ptyProcess.write('docker exec -it password_cracking_kali-client_1 bash\r');
+    ptyProcess.write('clear')
     ws.on('message', command => {
         ptyProcess.write(command);
     })
