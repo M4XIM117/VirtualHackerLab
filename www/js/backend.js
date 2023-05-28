73bc4c18c31520a7edc5aa96e9693ac13c05b4c6
Maxim Schmidt <maxim.schmidt@student.reutlingen-university.de>
2023-05-28 15:10:29 +0200

    Es funktioniert

--- www/js/backend.js
+++ www/js/backend.js
@@ -1,30 +1,67 @@
-const WebSocket = require('ws')
-var os = require('os');
-var pty = require('node-pty');
+const WebSocket = require('ws');
+const os = require('os');
+const pty = require('node-pty');
 
-const wss = new WebSocket.Server({ port: 6060 })
+const wss = new WebSocket.Server({ port: 6060 });
+console.log("Socket is up and running...");
 
-console.log("Socket is up and running...")
+class Terminal {
+  constructor(id, shell, cwd, env) {
+    this.id = id;
+    this.shell = shell;
+    this.cwd = cwd;
+    this.env = env;
+    this.ptyProcess = null;
+  }
 
-var shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
-var ptyProcess = pty.spawn(shell, [], {
-    name: 'xterm-color',
-    uid: 1000,
-    gid: 1000,
-    cwd: "/home/student/",
-    env: process.env,
-});
-wss.on('connection', ws => {
-    console.log("new session")
-    //ptyProcess.write('docker exec -it password_cracking_kali-client_1 bash\r');
-    //ptyProcess.write('clear\r')
-    ws.on('message', command => {
-        ptyProcess.write(command);
-    })
-
-    ptyProcess.on('data', function (data) {
-        ws.send(data)
-        console.log(data);
-
+  start(ws) {
+    this.ptyProcess = pty.spawn(this.shell, [], {
+      name: 'xterm-color',
+      cwd: this.cwd,
+      env: this.env
     });
-})
+
+    this.ptyProcess.on('data', data => {
+      ws.send(JSON.stringify({ terminalId: this.id, message: data }));
+    });
+
+  }
+
+  stop() {
+    if (this.ptyProcess) {
+      this.ptyProcess.kill();
+      this.ptyProcess = null;
+    }
+  }
+}
+
+const terminals = new Map();
+
+wss.on('connection', ws => {
+    ws.on('message', data => {
+        const { terminalId, command } = JSON.parse(data);
+        if (!(terminals.has(terminalId))) {
+            const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
+            const cwd = "/home/student/";
+            const env = process.env;
+        
+            const terminal = new Terminal(terminalId, shell, cwd, env);
+            terminals.set(terminalId, terminal);
+        
+            terminal.start(ws);
+            terminal.ptyProcess.write(command + '\r');
+            terminal.ptyProcess.write("clear\r")
+        } else {
+            const terminal = terminals.get(terminalId); 
+            if (command) {
+                terminal.ptyProcess.write(command + '\r');
+            }
+        }      
+    });
+
+    ws.on('close', () => {
+        terminals.forEach(terminal => {
+            terminal.stop();
+        });
+    });
+});
--- www/js/frontend.js
+++ www/js/frontend.js
@@ -1,81 +1,103 @@
+class Terminal {
+  constructor(element, startupCommand) {
+    this.element = element;
+    this.startupCommand = startupCommand;
+    this.term = null;
+    this.terminalId = null;
+    this.command = ''
+  }
+  
 
-const socket = new WebSocket("ws://localhost:6060");
-
-var term = new window.Terminal({
-    cursorBlink: true
-});
-
-function init() {
-    if (term._initialized) {
-        return;
-    }
-
-    term._initialized = true;
-
-    term.prompt = () => {
-        term.write('\r\n$ ');
-    };
-    prompt(term);
-
-    term.onData(e => {
-        switch (e) {
-            case '\u0003': // Ctrl+C
-                term.write('^C');
-                prompt(term);
-                break;
-            case '\r': // Enter
-                runCommand(term, command);
-                command = '';
-                break;
-            case '\u007F': // Backspace (DEL)
-                // Do not delete the prompt
-                if (term._core.buffer.x > 2) {
-                    term.write('\b \b');
-                    if (command.length > 0) {
-                        command = command.substr(0, command.length - 1);
-                    }
-                }
-                break;
-            case '\u0009':
-                console.log('tabbed', output, ["dd", "ls"]);
-                break;
-            default:
-                if (e >= String.fromCharCode(0x20) && e <= String.fromCharCode(0x7E) || e >= '\u00a0') {
-                    command += e;
-                    term.write(e);
-                }
-        }
+  initialize() {
+    this.term = new window.Terminal({
+      cursorBlink: true
     });
-}
+    this.term.open(this.element);
+    const forbiddenCommands = ["exit", "sudo shutdown", /^rm(\s.*)?$/i, "reboot"];
 
-function clearInput(command) {
-    var inputLengh = command.length;
-    for (var i = 0; i < inputLengh; i++) {
-        term.write('\b \b');
+    this.term.prompt = () => {
+      this.term.write('\r\n$ ');
+    };
+
+    this.term._initialized = false;
+
+    this.term.onData(e => {
+      switch (e) {
+        case '\u0003': // Ctrl+C
+          this.term.write('^C');
+          this.term.prompt();
+          break;
+        case '\r': // Enter
+          this.runCommand(this.term, );
+          this.command = ''
+          break;
+        case '\u007F': // Backspace (DEL)
+          // Do not delete the prompt
+          if (this.term._core.buffer.x > 2) {
+            this.term.write('\b \b');
+            if (this.command.length > 0) {
+                this.command = this.command.substr(0, this.command.length - 1);
+            }
+        }
+          break;
+        default:
+          if (e >= String.fromCharCode(0x20) && e <= String.fromCharCode(0x7E) || e >= '\u00a0') {
+            this.term.write(e);
+            this.command += e;
+          }
+      }
+    });
+
+    this.initializeWebSocket();
+  }
+
+  initializeWebSocket() {
+    const socket = new WebSocket("ws://localhost:6060");
+    socket.onopen = () => {
+      this.sendTerminalId();
+    };
+
+    socket.onmessage = event => {
+      const data = JSON.parse(event.data);
+      if (data.terminalId === this.terminalId) {
+        this.term.write(data.message);
+        this.term.prompt();
+      }
     }
-}
-function prompt(term) {
-    command = '';
-    term.write('\r\n$ ');
-}
-socket.onmessage = (event) => {
-    term.write(event.data);
-}
 
-function runCommand(term, command) {
-    if (command.length > 0) {
-        clearInput(command);
+    this.socket = socket;
+  }
 
-        // Liste verbotener Befehle
-        const forbiddenCommands = ["exit", "sudo shutdown", /^rm(\s.*)?$/i, "reboot"];
+  sendTerminalId() {
+    this.socket.send(JSON.stringify({ terminalId: this.terminalId, command: this.startupCommand }));
+  }
 
-        // Check if the entered command is forbidden
-        if (forbiddenCommands.includes(command)) {
-            term.write(`Command "${command}" is not allowed.\r\n`);
-        } else {
-            socket.send(command + '\n');
+  runCommand() { 
+    // const line = this.term._core.buffer.active.getLine(this.term._core.buffer.ybase + this.term._core.buffer.y);
+    // const command = line.translateToString().trim();
+    const command = this.command.trim();
+  
+    if (this.command.length > 0) {
+      if (forbiddenCommands.includes(this.command)) {
+        this.term.write(`Command "${this.command}" is not allowed.\r\n`);
+            this.command = '';
+      } else {
+          this.term.prompt();
+          this.term.write('\r\n');
+          this.socket.send(JSON.stringify({ terminalId: this.terminalId, command: this.command }));
+          this.command = '';
         }
     }
+  }
 }
 
-init();
+document.addEventListener('DOMContentLoaded', () => {
+  const terminalElements = document.getElementsByClassName('vhlterminal');
+  for (let i = 0; i < terminalElements.length; i++) {
+    const element = terminalElements[i];
+    const startupCommand = element.getAttribute('data-startup-command');
+    const terminal = new Terminal(element, startupCommand);
+    terminal.terminalId = i;
+    terminal.initialize();
+  }
+});
