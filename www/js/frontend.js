2d7068362e3e2dfd83f060a88072c0388f18035a
Maxim Schmidt <maxim.schmidt@student.reutlingen-university.de>
2023-06-07 17:17:51 +0200

    Jetzt

--- www/js/frontend.js
+++ www/js/frontend.js
@@ -50,9 +50,12 @@ class Terminal {
             this.term.write('\b \b');
             this.term.write("\x1b[1A\x1b[1000C");
             this.command = this.command.substring(0, this.command.length - 1);
+          } else if (this.term._core.buffer.x > 2 && this.command.length > 0) {
+              this.term.write('\b \b');
+              this.command = this.command.substring(0, this.command.length - 1);
           } else if (this.term._core.buffer.x <= 2 && this.command.length > 0) {
-            this.term.write('\b \b');
-            this.command = this.command.substring(0, this.command.length - 1);
+              this.term.write('\b \b');
+              this.command = this.command.substring(0, this.command.length - 1);
           } 
           break;
         default:
